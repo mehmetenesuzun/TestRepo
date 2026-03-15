03738a943ef653d50e9d270ccaf0c9f6ce98a06d
Enes <mehmetenesuzun@outlook.com>
2026-03-15 13:16:12 +0300

    Update test.php

--- test.php
+++ test.php
@@ -12,6 +12,8 @@ $query = "SELECT * FROM users WHERE username = '$username' AND password = '$pass
 
 $result = $conn->query($query);
 
+echo
+
 if ($result->num_rows > 0) {
     echo "Login başarılı";
 } else {
