672accc99bfeb622dfd51113c011b7180a4bc102
Enes <mehmetenesuzun@outlook.com>
2026-03-17 15:18:13 +0300

    Update and rename test.html to test.php

---
+++ test.php
@@ -0,0 +1,23 @@
+<?php
+$conn = new mysqli("localhost", "root", "", "testdb");
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+// ❌ Kullanıcı girdisi direkt sorguya ekleniyor
+$query = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
+
+$result = $conn->query($query);
+
+if ($result->num_rows > 0) {
+    echo "Login başarılı";
+} else {
+    echo "Login başarısız";
+}
+?>
+
+<form method="POST">
+    <input type="text" name="username" placeholder="username">
+    <input type="password" name="password" placeholder="password">
+    <button type="submit">Login</button>
+</form>
